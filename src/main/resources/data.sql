ad70afb99a0004d8d9b0dfe312aefd7367a56b87
AlainCarmona <alaincarmonahdz@gmail.com>
2024-01-16 11:29:55 -0600

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Employee(id, name, address) VALUES (1, 'RRHH', 'Av. Mario Colin 27');
+INSERT INTO Employee(id, name, address) VALUES (2, 'TI', 'Av. Mario Colin 27');
+INSERT INTO Employee(id, name, address) VALUES (3, 'Finanzas', 'Av. Mario Colin 25');
+
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (1232434, 56745764756423, 'David', 'Moreno', 25, 2);
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (12384534, 56555764756423, 'Alain', 'Carmona', 37, 2);
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (12384764, 56555764756423, 'César', 'Ordoñez', 40, 3);
