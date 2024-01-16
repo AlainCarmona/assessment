b36afd37b46e50073f1eaa362210683cdc8b50be
AlainCarmona <alaincarmonahdz@gmail.com>
2024-01-16 11:55:35 -0600

    Fixes for .sql scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO Employee(id, name, address) VALUES (1, 'RRHH', 'Av. Mario Colin 27');
-INSERT INTO Employee(id, name, address) VALUES (2, 'TI', 'Av. Mario Colin 27');
-INSERT INTO Employee(id, name, address) VALUES (3, 'Finanzas', 'Av. Mario Colin 25');
+INSERT INTO Department(id, name, address) VALUES (1, 'RRHH', 'Av. Mario Colin 27');
+INSERT INTO Department(id, name, address) VALUES (2, 'TI', 'Av. Mario Colin 27');
+INSERT INTO Department(id, name, address) VALUES (3, 'Finanzas', 'Av. Mario Colin 25');
 
-INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (1232434, 56745764756423, 'David', 'Moreno', 25, 2);
-INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (12384534, 56555764756423, 'Alain', 'Carmona', 37, 2);
-INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (12384764, 56555764756423, 'César', 'Ordoñez', 40, 3);
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (1232434, 5674423, 'David', 'Moreno', 25, 2);
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (2384534, 5664203, 'Alain', 'Carmona', 37, 2);
+INSERT INTO Employee(employeeNumber, accountNumber, firstName, lastName, age, department) VALUES (1384764, 5656423, 'César', 'Ordoñez', 40, 3);
